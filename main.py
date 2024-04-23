72909f50802919b0f0f9a14eac580a593c704700
AmaniWilliams <amanivrede@hotmail.com>
2024-04-23 12:45:35 +0200

    added the add child button no function

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ window.geometry("500x400")
 frame1 = CTkFrame(master= window, fg_color= "#33FF68", border_color= "#07BE37", border_width= 2
                   , corner_radius= 25, width= 200, height= 75)
 frame1.place(anchor= "nw", relx= 0.07, rely= 0.06)
-entry1 = CTkEntry(master= frame1, placeholder_text= "Voer een naam in")
-entry1.place(anchor= "nw", rely= 0.2, relx= 0.1,)
+entry1 = CTkEntry(master= frame1, placeholder_text= "Voer een naam in", corner_radius= 32)
+entry1.place(anchor= "nw", rely= 0.2, relx= 0.1)
+
+add_frame_button = CTkButton(master= window, hover_color= "#08BB37",
+                             fg_color= "#01FF44", text= "Voeg kind toe")
+add_frame_button.place(anchor = "nw", relx= 0.1, rely= 0.3)
 
 
 
